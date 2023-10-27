1e8acb5413d117a5dc02723b0d433ae45b2c1467
Jorr1t <jrrtvrbm@gmail.com>
2023-10-27 13:09:59 +0200

    adding results works with objects

--- src/Components/Results/Results.js
+++ src/Components/Results/Results.js
@@ -2,21 +2,71 @@ import React from 'react';
 import './Results.css'
 
 function Results() {
+    
+    function showResult() {
+
+        const songs = [
+            {
+                artist: 'Elton John',
+                songTitle: 'Tiny Dancer',
+                album: 'Madman Across The Water'
+            },
+            {
+                artist: 'Run The Jewels',
+                songTitle: 'holy calamfuck',
+                album: 'RTJ4'
+            },
+            {
+                artist: 'Gorillaz',
+                songTitle: 'Last Living Souls',
+                album: 'Demon Days'
+            },
+            {
+                artist: 'Sticks',
+                songTitle: 'Solo',
+                album: 'STICKmatic'
+            },
+            {
+                artist: 'The Chemical Brothers',
+                songTitle: 'Snow',
+                album: 'Further'
+            },
+            {
+                artist: 'Kubus',
+                songTitle: 'Koppensnellers',
+                album: 'Buiten Westen'
+            }
+        ]
+
+        const list = songs.map((a) => 
+            (
+            <div>
+                <h3>{a.artist}</h3>
+                <p>{a.songTitle} | {a.album}</p>
+            </div>
+            )
+        );
+
+        const song = list.map((a) =>
+            (
+            <div className="song">
+                {a}
+                <div>
+                    <div className="alt">+</div>
+                </div>
+            </div>
+            )
+        );
+
+    return song;
+    }
+
     return (
         <div className="results">
             <div className="resultsBox">
                 <h2>Results</h2>
-                <div className="song">
-                    <div>
-                        <h3>Tiny Dancer</h3>
-                        <p>Elton John | Madman Across The Water</p>
-                    </div>
-                    <div>
-                        <div className="alt">+</div>
-                    </div>
-                </div>
+                {showResult()}
             </div>
-            
         </div>
     )
 }
